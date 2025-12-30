e29c8e27ce7872ea8f9a165918ca3cc23c0a8872
candy379 <152316031+candy379@users.noreply.github.com>
2025-12-30 14:07:08 +0530

    loops

---
+++ Conventions.java
@@ -0,0 +1,4 @@
+public class Conventions {
+    public static void main(String[] args){
+    }
+}
--- LoopsDemo.java
+++ LoopsDemo.java
@@ -1,8 +1,23 @@
 public class LoopsDemo {
     public static void main(String[] args){
 
-        for(int i=1;i<=5;i++){
-            System.out.println(i);
+//        for(int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+        for (int i=1;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+        for (int i=1;i<=10;i++) {
+            System.out.println("\nTable: "+i);
+
+            for(int j=1;j<=10;j++){
+                System.out.println(i+"X"+j+"="+i*j);
+
+            }
+            System.out.println();
         }
 
     }
